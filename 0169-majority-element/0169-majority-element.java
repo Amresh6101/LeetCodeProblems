1f9d0932ba05f6a91fe14039552f37ed43d06840
Amresh Ranjan <89856153+Amresh6101@users.noreply.github.com>
2025-12-07 22:41:23 +0530

    Time: 1 ms (99.84%), Space: 55.6 MB (23.65%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,20 +1,24 @@
 class Solution {
     public int majorityElement(int[] nums) {
-       int majority=nums[0];
+        // space complexity-0(n) using hashMap
+
+        int leader=nums[0];
         int vote=1;
         for(int i=1;i<nums.length;i++){
-
-            if(vote==0){
-                majority=nums[i];
+            if(leader==nums[i]){
                 vote++;
-            }
-            else if(majority==nums[i]){
-                vote++;
-            }
-            else{
-                vote--;
+            }else{
+                if(vote==0){
+                    leader=nums[i];
+                    vote++;
+                }
+                else{
+                    vote--;
+                }
             }
         }
-        return majority;
+        return leader;
+        
+        
     }
 }
