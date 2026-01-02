ed12912a6f6448b3ce5cced201fef1ccbb0f8562
Amresh Ranjan <89856153+Amresh6101@users.noreply.github.com>
2026-01-02 23:35:13 +0530

    Time: 6 ms (67.09%), Space: 46.1 MB (57.18%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -4,21 +4,41 @@ class Solution {
 
         // dynamic sliding window
         
-        // basic ds window pattern template  880/988 testcase passed
+        // basic ds window pattern template 
         int wSize=0;
         
-        for(int i=0;i<s.length();i++){
-            Set<Character> list=new  HashSet<>();
-            for(int j=i;j<s.length();j++){
-                if(list.contains(s.charAt(j))){
-                    break;
-                }
-                list.add(s.charAt(j));
-                wSize=Math.max(wSize,j-i+1);
+        // this is brute approach
+        // for(int i=0;i<s.length();i++){
+        //     Set<Character> list=new  HashSet<>();
+        //     for(int j=i;j<s.length();j++){
+        //         if(list.contains(s.charAt(j))){
+        //             break;
+        //         }
+        //         list.add(s.charAt(j));
+        //         wSize=Math.max(wSize,j-i+1);
 
+        //     }
+        // }
+
+        // return wSize; 
+
+
+        // optimization
+
+        int i=0,
+            j=0;
+        Set<Character> set=new HashSet<>();
+        while(j<s.length()){
+
+            while(set.contains(s.charAt(j))){
+                set.remove(s.charAt(i));
+                i++;
             }
-        }
+            set.add(s.charAt(j));
+            wSize=Math.max(wSize,j-i+1);
 
-        return wSize;     
+            j++;
+        }  
+        return wSize;      
     }
 }
