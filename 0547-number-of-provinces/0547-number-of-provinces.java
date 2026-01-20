f734077e91fa156adbf86dd874df7c6d8df8ce83
Amresh Ranjan <89856153+Amresh6101@users.noreply.github.com>
2026-01-20 22:57:42 +0530

    Time: 19 ms (6.07%), Space: 47.2 MB (67.11%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+
+        // first make List<List<Integer>> for graph representation
+        List<List<Integer>> adjList=new ArrayList<>();
+        for(int i=0;i<isConnected.length;i++){
+            adjList.add(new ArrayList<>());
+        }
+
+        for(int i=0;i<isConnected.length;i++){
+            for(int j=0;j<isConnected[i].length;j++){
+                if(isConnected[i][j]==1){
+                    adjList.get(i).add(j);
+                }
+            }
+        }
+        // print adjacency list
+        for (int i = 0; i < isConnected.length; i++) {
+            System.out.println(i + " -> " + adjList.get(i));
+        }
+
+        int[] visitedArray=new int[isConnected.length+1];
+        int count=0;
+        for(int i=0;i<isConnected.length;i++){
+            if(visitedArray[i]==0){
+                count++;
+                dfs(visitedArray,i,adjList);
+            }
+        }
+        return count;              
+    }
+    public void dfs(int[] visitedArray, int node, List<List<Integer>> adjList ){
+
+        visitedArray[node]=1;
+        for(int curr: adjList.get(node)){
+            if(visitedArray[curr]==0){
+                dfs(visitedArray,curr,adjList);
+            }
+        }
+
+    }
+}
