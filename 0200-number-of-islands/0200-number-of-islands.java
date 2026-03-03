1fcfd41c526fb4a84b3b9da9e7a873e83fa85b46
Amresh Ranjan <89856153+Amresh6101@users.noreply.github.com>
2026-03-03 18:45:52 +0530

    Time: 3 ms (86.86%), Space: 52.4 MB (46.35%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,38 +1,30 @@
 class Solution {
     public int numIslands(char[][] grid) {
+       int rows=grid.length;
+       int cols=grid[0].length;
+       int island=0;
 
-        int rows=grid.length;
-        int cols=grid[0].length;
-
-        int islands=0;
-
-        // now iterate over grid
-        // if found 1 we found island and do check their corrs neighbour
-
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(grid[i][j]=='1'){
-                    islands++;
-                    // do search for their neighbour
-                    dfs(grid,i,j,rows,cols);
-                }
+       for(int i=0;i<rows;i++){
+        for(int j=0;j<cols;j++){
+            if(grid[i][j]=='1'){
+                island++;
+                dfs(i,j,rows,cols,grid);
             }
         }
+       }
+       return island;
 
-        return islands;
     }
-    public void dfs(char[][] grid,int i,int j,int rows,int cols){
-        // base case
-        if(i<0 || i>=rows || j<0 || j>=cols || grid[i][j]=='0') return;
-
-        // now mark i,j as 0 to prevent futher visiting
+    public void dfs(int i,int j,int rows, int cols,char[][] grid){
+        // base condition
+        if(i<0 || i>=rows || j<0 || j>=cols || grid[i][j]=='0'){
+            return ;
+        }
         grid[i][j]='0';
 
-        // now explore surrounding
-
-        dfs(grid, i+1,j,rows,cols);
-        dfs(grid, i-1,j,rows,cols);
-        dfs(grid, i,j+1,rows,cols);
-        dfs(grid, i,j-1,rows,cols);
+        dfs(i+1,j,rows,cols,grid);
+        dfs(i-1,j,rows,cols,grid);
+        dfs(i,j+1,rows,cols,grid);
+        dfs(i,j-1,rows,cols,grid);
     }
 }
