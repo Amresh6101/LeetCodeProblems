07bb8de500be0c3f6bd8786c768541c8c5716e54
Amresh Ranjan <89856153+Amresh6101@users.noreply.github.com>
2026-04-26 00:14:47 +0530

    Time: 55 ms (43.78%), Space: 70.5 MB (41.75%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,47 +1,23 @@
 class Solution {
-    public int totalFruit(int[] fruits) {
+    public int totalFruit(int[] nums) {
+        Map<Integer,Integer> map=new HashMap<>();
+        int i=0;
+        int len=0,maxLen=0;
+        for(int j=0;j<nums.length;j++){
+            map.put(nums[j],map.getOrDefault(nums[j],0)+1);
 
-        int n=fruits.length;
-        int maxLen=0,i=0,j=0;
-        
-        Map<Integer,Integer> map1=new HashMap<>();
+            while(map.size()>=3){
+                int val1=map.get(nums[i]);
+                val1--;
+                map.put(nums[i],val1);
 
-        while(j<n){
-            map1.put(fruits[j],map1.getOrDefault(fruits[j],0)+1);
-
-            while(map1.size()>=3){
-                map1.put(fruits[i],map1.get(fruits[i])-1);
-                if(map1.get(fruits[i])==0){
-                    map1.remove(fruits[i]);
-                }
+                if(map.get(nums[i])==0) map.remove(nums[i]);
                 i++;
             }
-            maxLen=Math.max(maxLen,j-i+1);
-            j++;
+
+            len=j-i+1;
+            maxLen=Math.max(maxLen,len);
         }
-        // Set<Integer> set=new HashSet<>();
-        // while(j<n){
-        //     set.add(fruits[j]);
-        //     while(set.size()>2){
-        //         set.remove(fruits[i]);
-        //         i++;
-        //     }
-        //     maxLen=Math.max(maxLen,j-i+1);
-        //     j++;
-        // }
-        // we can not use set bcoze while remove all element remove ho rha hai hume sirf count reduce krna hai so that we are using hashMap to store count of each element;
-        // for(int i=0;i<n;i++){
-        //     Set<Integer> set=new HashSet<>();
-        //     for(int j=i;j<n;j++){
-        //         set.add(fruits[j]);
-        //         if(set.size()<=2){
-        //             maxLen=Math.max(maxLen,j-i+1);
-        //         }
-        //         else{
-        //             break;
-        //         }
-        //     }
-        // }
         return maxLen;
         
     }
