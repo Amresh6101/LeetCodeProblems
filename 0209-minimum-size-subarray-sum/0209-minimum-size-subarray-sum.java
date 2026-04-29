fcfdc7827dff99b8545be71aafd37d3cc84e0e1a
Amresh Ranjan <89856153+Amresh6101@users.noreply.github.com>
2026-04-29 17:06:01 +0530

    Time: 1 ms (99.94%), Space: 61.9 MB (70.68%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,54 +1,26 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
+        int res=Integer.MAX_VALUE;
 
-        // variable size sliding window hai
-        // size ka pta nhi hai
-
-        // brute approach
-        // tc=O(n*n)
-
-     
-
-        // for(int i=0;i<nums.length;i++){
-        //     int sum=0;
-        //     for(int j=i;j<nums.length;j++){
-        //         sum+=nums[j];
-        //         if(sum>=target){
-        //             windowSize=Math.min(windowSize,j-i+1);
-        //             break;
-        //         }
-        //     }
-        // }
-        // if(windowSize==Integer.MAX_VALUE){
-        //     return 0;
-        // }
-        // else{
-        //     return windowSize;
-        // }
-
-        int windowSize=Integer.MAX_VALUE;
+        int i=0,j=0;
         int sum=0;
 
-        int i=0;
-        int j=0;
-
         while(j<nums.length){
-
-            sum=sum+nums[j];   
-            while (sum>=target){
-                windowSize=Math.min(windowSize,j-i+1);
+            sum+=nums[j];
+            
+            while(sum>=target){
+                res=Math.min(res,j-i+1);
                 sum=sum-nums[i];
                 i++;
             }
+
             j++;
         }
 
-        if(windowSize==Integer.MAX_VALUE){
-            return 0;
-        }
-        else{
-            return windowSize;
-        }
-        
+        if(res==Integer.MAX_VALUE) return 0;
+        return res;
+
+
+
     }
 }
