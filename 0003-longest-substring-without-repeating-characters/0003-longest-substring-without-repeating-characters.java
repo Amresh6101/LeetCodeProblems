f9a19069bb9613571bcc18802df433b458a837f1
Amresh Ranjan <89856153+Amresh6101@users.noreply.github.com>
2026-06-01 00:06:14 +0530

    Time: 7 ms (36.02%), Space: 46.6 MB (28.02%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,44 +1,20 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
 
+         Set<Character> set=new HashSet<>();
+         int i=0;
+         int j=0;
+         int res=0;
 
-        // dynamic sliding window
-        
-        // basic ds window pattern template 
-        int wSize=0;
-        
-        // this is brute approach
-        // for(int i=0;i<s.length();i++){
-        //     Set<Character> list=new  HashSet<>();
-        //     for(int j=i;j<s.length();j++){
-        //         if(list.contains(s.charAt(j))){
-        //             break;
-        //         }
-        //         list.add(s.charAt(j));
-        //         wSize=Math.max(wSize,j-i+1);
-
-        //     }
-        // }
-
-        // return wSize; 
-
-
-        // optimization
-
-        int i=0,
-            j=0;
-        Set<Character> set=new HashSet<>();
-        while(j<s.length()){
-
+         while(j<s.length()){
             while(set.contains(s.charAt(j))){
                 set.remove(s.charAt(i));
                 i++;
             }
             set.add(s.charAt(j));
-            wSize=Math.max(wSize,j-i+1);
-
+            res=Math.max(res,j-i+1);
             j++;
-        }  
-        return wSize;      
+         }
+         return res;
     }
 }
