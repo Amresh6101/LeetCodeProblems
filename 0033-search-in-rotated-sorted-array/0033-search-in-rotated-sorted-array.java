6dabb4a88b2f05bd29e9a836fe6ea66659c7ec3f
Amresh Ranjan <89856153+Amresh6101@users.noreply.github.com>
2026-06-13 22:26:13 +0530

    Time: 0 ms (100%), Space: 43.9 MB (45.86%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,37 +1,44 @@
 class Solution {
     public int search(int[] arr, int target) {
-        // we have to find which part of the array is sorted it is either left and right.
 
-        int n=arr.length;
+
         int low=0;
-        int high=n-1;
+        int high=arr.length-1;
 
+        int res=-1;
 
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(arr[mid]==target) return mid;
 
-            // if arr[mid] not equal to target we have to find which part is sorted and not sorted.
-
-            else if(arr[mid]<=arr[high]){
-                 // check is target is present in their search space.
-                 if(arr[mid]<target && target<=arr[high]){
-                    low=mid+1;
-                 }
-                 else{
-                    high=mid-1;
-                 }
+            if(arr[mid]==target){
+                return mid;
             }
-            else{
-                if(arr[low]<=target && target<=arr[mid]){
+            // check which part is sorted
+            if(arr[low]<=arr[mid]){
+                // left part sorted
+                // now check whether target present in this part or not
+
+                if(target>=arr[low] && target<arr[mid]){
                     high=mid-1;
                 }
                 else{
                     low=mid+1;
                 }
+
+            }
+            else{
+                // right part sorted
+
+                if(target>arr[mid] && target<=arr[high]){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
+
             }
         }
-        return -1;
-
+        return res;
+        
     }
 }
